3a9bbcc33ccb25c3eaff6a30cf3acb1abb893fa8
Al Mamon <shikderr112@gmail.com>
2024-10-26 23:27:14 +0600

    fixed navbar

--- src/app/component/LandingPage/Banner/Banner.tsx
+++ src/app/component/LandingPage/Banner/Banner.tsx
@@ -1,10 +1,11 @@
+"use client";
 import React from "react";
 import BannerImg from "@/assets/images/slider1.jpg";
 import HeartImg from "@/assets/images/heart-icon.png";
-import LevelImg from "@/assets/images/title-bg.png";
+
 import SignitureImg from "@/assets/images/slider_sign_img.webp";
 import Image from "next/image";
-
+import { motion } from "framer-motion";
 const Banner = () => {
   return (
     <div className="">
@@ -16,23 +17,38 @@ const Banner = () => {
       >
         <div className="">
           <div className="py-20 xl:ml-[700px] lg:ml-[600px] px-12">
-            <div className="inline-flex gap-2 items-center">
+            <motion.div
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+              className="inline-flex gap-2 items-center"
+            >
               <Image src={HeartImg} alt="heart"></Image>
               <p>
                 <span className="text-xl">Hello!</span> i am artist
               </p>
-            </div>
-            <div className="mt-2 relative">
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+              className="mt-2 relative"
+            >
               <h2 className="lg:text-6xl md:text-6xl relative  text-5xl font-semibold xl:w-[450px] lg:w-[450px] md:w-[500px] w-[300px]">
                 Best <span className="z-10 ">online</span> platform for painting
               </h2>
-              <div className="absolute top-4 left-36 -rotate-6 -z-30">
+              {/* <div className="absolute top-4 left-36 -rotate-6 -z-10">
                 <Image src={LevelImg} alt="heart"></Image>
-              </div>
-            </div>
-            <div className="w-[180px] mt-8">
+              </div> */}
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -80 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              className="w-[180px] mt-8"
+            >
               <Image src={SignitureImg} alt="heart"></Image>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
